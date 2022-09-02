bfb5d119b3cf1281a1d80b5ac7f073ecde8f313c
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-09-02 11:15:03 +0300

    new solution

--- src/main/kotlin/com/serega/leetcode/easy/ValidParentheses.kt
+++ src/main/kotlin/com/serega/leetcode/easy/ValidParentheses.kt
@@ -1,4 +1,31 @@
 package com.serega.leetcode.easy
 
-class ValidParentheses {
+fun main() {
+    "(){}".isValidParentheses().apply(::println)
+    "(){".isValidParentheses().apply(::println)
+    "(){}[](){}()".isValidParentheses().apply(::println)
+    "(){}[](){}()]".isValidParentheses().apply(::println)
+}
+
+fun String.isValidParentheses(): Boolean {
+    val p = '(' to ')'
+    val b = '[' to ']'
+    val k = '{' to '}'
+
+    for ((index, value) in withIndex()) {
+        if (index % 2 == 0) {
+            val nextChar = getOrNull(index + 1)
+            if (!when (value) {
+                    p.first -> p.second == nextChar
+                    b.first -> b.second == nextChar
+                    k.first -> k.second == nextChar
+                    else -> false
+                }
+            ) {
+                return false
+            }
+        }
+    }
+
+    return true
 }
