042845fc6c9d3870f331caa00902dd46d9d0898f
Serega <26937163+serega-fighter@users.noreply.github.com>
2022-09-17 11:09:05 +0300

    Test methods

--- src/main/kotlin/com/serega/leetcode/easy/RemoveDuplicates.kt
+++ src/main/kotlin/com/serega/leetcode/easy/RemoveDuplicates.kt
@@ -3,5 +3,12 @@ package com.serega.leetcode.easy
 fun removeDuplicates(ar: IntArray) = ar.distinct().mapIndexed { i, n -> ar[i] = n }.size
 
 fun main() {
+    val testArr = intArrayOf(5, 3, 1, 2, 2, 3, 4, 3, 1, 5)
+
+    val distinctArray = testArr.distinct()
+    println(distinctArray)
+
+    println(intArrayOf(9, 5, 3, 1, 132).mapIndexed { index, value -> index + 1 })
+
     println(removeDuplicates(intArrayOf(1, 1, 2, 3, 4, 5, 5, 5, 6, 7)))
 }
